aafbaa3eb9f58c78dd5c3fc17bfdc817e5673c7e
Hafsa <k225161@nu.edu.pk>
2025-03-25 22:29:37 +0500

    DB Connection

---
+++ DB_Connection/Connection.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+try: 
+    conn = mysql.connector.connect (
+        host = "localhost",
+        user = "root",
+        password = "abc123",
+        database = "CN_PROJECT"
+    )
+
+    if (conn.is_connected): 
+        print("Database is connected successfully!")
+        #Remove it after
+        conn.close()
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
