b49679fe278a784d68f24adf53e1a169169cd22c
Hafsa <k225161@nu.edu.pk>
2025-03-26 13:23:25 +0500

    DB Connection

--- DB_Connection/Connection.py
+++ DB_Connection/Connection.py
@@ -1,17 +1,18 @@
 import mysql.connector
 
-try: 
-    conn = mysql.connector.connect (
-        host = "localhost",
-        user = "root",
-        password = "abc123",
-        database = "CN_PROJECT"
-    )
+def createConnection():
+    try: 
+        conn = mysql.connector.connect (
+            host = "localhost",
+            user = "root",
+            password = "abc123",
+            database = "CN_PROJECT"
+        )
 
-    if (conn.is_connected): 
-        print("Database is connected successfully!")
-        #Remove it after
-        conn.close()
+        if (conn.is_connected): 
+            print("Database is connected successfully!")
+            #Remove it after
+            conn.close()
 
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
