644066b5fe33407be392b6b97bd19450aa8aaf05
Hafsa <k225161@nu.edu.pk>
2025-04-01 17:45:52 +0500

    Login

--- Authentication/Login.py
+++ Authentication/Login.py
@@ -2,3 +2,10 @@ import mysql.connector
 import bcrypt
 import sys
 import os
+from DB_Connection.Connection import createConnection
+
+def LoginUser():
+    conn = createConnection()
+
+    if conn is None:
+        print
