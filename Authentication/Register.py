e2b710f41bc06ed549df212fc19cab1c62cf048b
Hafsa <k225161@nu.edu.pk>
2025-04-01 17:24:14 +0500

    User Registration

--- Authentication/Register.py
+++ Authentication/Register.py
@@ -3,7 +3,8 @@ from DB_Connection.Connection import createConnection
 import bcrypt
 
 #Change Phone Number and Profile Picture later
-def RegisterUser(full_name, username, email, password, phone_number = None, profile_picture = None):
+# def RegisterUser(full_name, username, email, password, phone_number = None, profile_picture = None):
+def RegisterUser():
     conn = createConnection()
     if conn is None:
         print("Database Connection Failed!")
@@ -12,6 +13,14 @@ def RegisterUser(full_name, username, email, password, phone_number = None, prof
     cursor = conn.cursor()
 
     try:
+        full_name = input("Enter Full Name: ").strip()
+        username = input("Enter Username: ").strip()
+        email = input("Enter Email: ").strip()
+        password = input("Enter Password: ").strip()
+        #Change later (Phone Number and Profile Picture)
+        phone_number = input("Enter Phone Number (Press Enter to skip): ").strip()
+        profile_picture = input("Enter URL for profile picture(Press Enter to skip): ").strip()
+
         cursor.execute("SELECT * FROM USERS WHERE email = %s", (email, ))
         existingEmail = cursor.fetchone()
 
@@ -45,9 +54,11 @@ def RegisterUser(full_name, username, email, password, phone_number = None, prof
 
 #Testing --Change once it works
 if __name__ == "__main__":
-    full_name = "Hafsa Salman"
-    username = "__hafsay"
-    email = "k225161@nu.edu.pk"
-    password = "22K-5161"
+    # full_name = "Hafsa Salman"
+    # username = "__hafsay"
+    # email = "k225161@nu.edu.pk"
+    # password = "22K-5161"
 
-    RegisterUser(full_name, username, email, password)
+    # RegisterUser(full_name, username, email, password)
+
+    RegisterUser()
