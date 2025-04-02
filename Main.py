0be4eba569c44fb6e10b58846a67ed3a749fb184
Hafsa <k225161@nu.edu.pk>
2025-04-02 10:31:22 +0500

    Main

---
+++ Main.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+def main():
+    while True:
+        print("Welcome to Chat Portal")
+        print("1. Register")
+        print("2. Login")
+        
+        choice = input("Choice: ").strip()
+
+        if choice == "1":
+            print("Registration Page!")
+            subprocess.run(["python", os.path.join(os.path.dirname(__file__), "Authentication", "Register.py")])
+
+        elif choice == "2":
+            print("Login Page!")
+            subprocess.run(["python", os.path.join(os.path.dirname(__file__), "Authentication", "Login.py")])
+
+        else:
+            print("Invalid choice!")
+
+if __name__ == "__main__":
+    main()
