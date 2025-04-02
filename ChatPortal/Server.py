f9f5d2c071954da811ea4f6692d0360a0b180d19
Hafsa <k225161@nu.edu.pk>
2025-04-02 21:32:43 +0500

    Server Chat

--- ChatPortal/Server.py
+++ ChatPortal/Server.py
@@ -9,5 +9,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from DB_Connection.Connection import createConnection
 
-clients = {}
+session = {}
 
+def MessageHistory():
+    conn = createConnection()
+
+    if conn is None:
+        return[]
+    
+    cursor = conn.cursor()
