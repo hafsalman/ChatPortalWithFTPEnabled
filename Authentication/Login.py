7e9f8ce9d7cf33b842aae0691b809fb097c7f8e1
Hafsa <k225161@nu.edu.pk>
2025-04-02 10:45:39 +0500

    User Login

--- Authentication/Login.py
+++ Authentication/Login.py
@@ -2,6 +2,9 @@ import mysql.connector
 import bcrypt
 import sys
 import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from DB_Connection.Connection import createConnection
 
 def LoginUser():
