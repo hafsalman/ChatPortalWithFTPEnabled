da071b5842360386c20ed5374d89fa327b839a32
Hafsa <k225161@nu.edu.pk>
2025-04-25 10:36:27 +0500

    Chat Portal

--- ChatPortal/Client.py
+++ ChatPortal/Client.py
@@ -23,34 +23,33 @@ def ShowHistory(usernamr):
         cursor = conn.cursor()
 
         cursor.execute("""SELECT sender, reciever, messafe, m_time FROM MESSAGES WHERE (sender = %s AND receiver = 'server') OR (sender = 'server' AND receiver = %s) ORDER BY  m_time ASC""", (username, username))
+        for row in cursor.fetchall():
+            sender, receiver, message, timestamp = row
+            print(f"[{timestamp.strftime('%Y-%m-%d %H:%M:%S')}] {sender}: {message}")
+
+    except Exception as e:
+        print("History couldn't be loaded!")
+
+    finally:
+        cursor.close()
+        conn.close()
+
+def ReceiveMessage(sock):
+    while True:
+        try:
+            message = sock.recv(1024).decode('utf-8')
+
+            if message:
+                print(f"\n{message} \nClient: ", end="", flush=True)
+
+            else:
+                break
+
+        except:
+            print("Connection Lost!")
+            break
 
 
-#         print("\n📜 Message History with Server:")
-#         print("-----------------------------------")
-#         for row in cursor.fetchall():
-#             sender, receiver, message, timestamp = row
-#             print(f"[{timestamp.strftime('%Y-%m-%d %H:%M:%S')}] {sender}: {message}")
-
-#         print("-----------------------------------\n")
-
-#     except Exception as e:
-#         print("❌ Error loading history:", e)
-#     finally:
-#         cursor.close()
-#         conn.close()
-
-# # === Receive Messages ===
-# def receive_messages(sock):
-#     while True:
-#         try:
-#             message = sock.recv(1024).decode('utf-8')
-#             if message:
-#                 print(f"\n{message}\nClient: ", end="", flush=True)
-#             else:
-#                 break
-#         except:
-#             print("\n❌ Connection lost.")
-#             break
 
 # # === Main Chat Function ===
 # def start_chat(username):
@@ -91,5 +90,3 @@ def ShowHistory(usernamr):
 #         print("Usage: python Client.py <username>")
 #     else:
 #         start_chat(sys.argv[1])
-
-
