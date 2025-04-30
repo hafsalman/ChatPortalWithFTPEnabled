e4b57cb67237abe98ba3dbdbb27c7ef31d908b52
Hafsa <k225161@nu.edu.pk>
2025-04-30 11:09:33 +0500

    Main

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from Authentication.Login import login_user
-from Authentication.Register import register_user  # assuming you have similar register function
+from Authentication.Register import register_user
 
 app = FastAPI()
 
